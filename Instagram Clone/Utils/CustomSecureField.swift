cc634356033e06669b7291104bd76b6c130d65b0
Kodirbek <qodirbekkhamzaev@gmail.com>
2023-02-23 23:08:08 +0900

    Login View complete

---
+++ Instagram Clone/Utils/CustomSecureField.swift
@@ -0,0 +1,43 @@
+//
+//  CustomSecureField.swift
+//  Instagram Clone
+//
+//  Created by Abduqodir's MacPro on 2023/02/23.
+//
+
+import SwiftUI
+
+struct CustomSecureField: View {
+  
+  @Binding var text: String
+  let placeholder: Text
+  
+  
+  var body: some View {
+    ZStack(alignment: .leading) {
+      
+      if text.isEmpty {
+        placeholder
+          .foregroundColor(Color(.init(white: 1, alpha: 0.8)))
+          .padding(.leading, 40)
+      }
+      
+      HStack {
+        Image(systemName: "lock")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 20, height: 20)
+          .foregroundColor(.white)
+          
+        SecureField("", text: $text)
+      } //: HStack
+    } //: ZStack
+  }
+}
+
+struct CustomSecureField_Previews: PreviewProvider {
+  static var previews: some View {
+    CustomSecureField(text: .constant(""), placeholder: Text("Password"))
+      .preferredColorScheme(.dark)
+  }
+}
--- Instagram Clone/View/Authentication/LoginView.swift
+++ Instagram Clone/View/Authentication/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+  
+  @State private var email = ""
+  @State private var password = ""
+  
   var body: some View {
     NavigationView {
       ZStack {
@@ -19,23 +23,75 @@ struct LoginView: View {
             .resizable()
             .scaledToFit()
             .frame(width: 220, height: 100)
-            .padding(.top, 70)
+            .padding(.top, 40)
           
           // email field
-          
-          // password field
+          VStack(spacing: 20) {
+            CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+              .padding()
+              .background(Color(.init(white: 1, alpha: 0.15)))
+              .cornerRadius(10)
+              .foregroundColor(.white)
+              .padding(.horizontal, 32)
+            
+            // password field
+            CustomSecureField(text: $password, placeholder: Text("Password"))
+              .padding()
+              .background(Color(.init(white: 1, alpha: 0.15)))
+              .cornerRadius(10)
+              .foregroundColor(.white)
+              .padding(.horizontal, 32)
+          }
+          .padding(.top, 50)
           
           // forgot password
+          HStack {
+            Spacer()
+            
+            Button {
+              // some action
+            } label: {
+              Text("Forgot Password?")
+                .font(.system(size: 13, weight: .semibold))
+                .foregroundColor(.white)
+                .padding(.top)
+                .padding(.trailing, 32)
+            }
+          } //: HStack
           
           // sign in
-          
-          // go to sign in
-          
+          Button {
+            // some action
+          } label: {
+            Text("Sign In")
+              .font(.headline)
+              .foregroundColor(.white)
+              .frame(width: 360, height: 50)
+              .background(.purple)
+              .clipShape(Capsule())
+              .padding()
+          }
           
           Spacer()
-        }
-      }
-    }
+
+          // go to sign in
+          HStack {
+            Text("Don't have an account?")
+              .font(.system(size: 14, weight: .semibold))
+            
+            Button {
+              // Sign up action
+            } label: {
+              Text("Sign Up")
+                .font(.system(size: 14, weight: .bold))
+            }
+          } //: HStack
+          .foregroundColor(.white)
+          .padding(.bottom, 25)
+
+        } //: VStack
+      } //: ZStack
+    } //: NavigationView
   }
 }
 
