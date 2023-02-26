d048c19243e15d3ef06c2e29af44d0ac8cb7acc5
Kodirbek <qodirbekkhamzaev@gmail.com>
2023-02-26 23:39:30 +0900

    Next commit

---
+++ Instagram Clone/ViewModel/Authentication/AuthViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  AuthViewModel.swift
+//  Instagram Clone
+//
+//  Created by Abduqodir's MacPro on 2023/02/26.
+//
+
+import SwiftUI
+import Firebase
+
+class AuthViewModel: ObservableObject {
+  
+  func login() {
+    
+  }
+  
+  func register() {
+    
+  }
+  
+  func signOut() {
+    
+  }
+  
+  func fetchUser() {
+    
+  }
+}
